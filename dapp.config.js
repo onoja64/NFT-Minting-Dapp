63d0aee45d271bb193264428a05235851d9ff96b
onoja64 <corneliussamuel64@gmail.com>
2022-07-20 05:08:15 +0100

    commit1

---
+++ dapp.config.js
@@ -0,0 +1,10 @@
+const config = {
+  title: 'BestApe Dapp',
+  description: 'A showcase dapp which is built for you to create your own',
+  contractAddress: '0x019f3b4c281dA5bad2DC70A0B19F578b542E46B3',
+  maxMintAmount: 10,
+  presaleMaxMintAmount: 3,
+  price: 0.01
+}
+
+export { config }
